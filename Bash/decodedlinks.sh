89b7835bd314a21deee48dfa5a8ea76c2168665b
Jared Johnston <jazzab@gmail.com>
2015-09-16 21:02:44 +1000

    Create youtube-dl script
    Created youtube-dl script (by inserting "youtube-dl -x " to the decoded
    links). Then called the script.

--- Bash/decodedlinks.sh
+++ Bash/decodedlinks.sh
@@ -1,10 +1,6 @@
-http://www.youtube.com/watch?v=UIK-Sdrzpqc
-
-http://www.metrolyrics.com/wolf-like-me-lyrics-tv-on-the-radio.html
-
-http://www.youtube.com/watch?v=JGhoLcsr8GA
-
-http://www.youtube.com/watch?v=RCHCcoPIQpk
-
-http://www.youtube.com/watch?v=jDplwu9kH9Y
-
+youtube-dl -x http://www.metrolyrics.com/wolf-like-me-lyrics-tv-on-the-radio.html
+youtube-dl -x 
+youtube-dl -x <!DOCTYPE
+youtube-dl -x 
+youtube-dl -x http://www.youtube.com/watch?v=FCmPFrBtAaM
+youtube-dl -x 
--- Bash/downloadtags.sh
+++ Bash/downloadtags.sh
@@ -1,33 +1,32 @@
 #!/usr/bin/env bash
 # some good info about sed: https://unix.stackexchange.com/questions/32907/what-characters-do-i-need-to-escape-when-using-sed-in-a-sh-script
 
-#todo remove curllinks.sh, curllinks.sh--, encodedlinks.sh and encodedlinks.sh-- files.
+#todo remove curllinks.sh, curllinks.sh--, encodedlinks.txt and encodedlinks.txt-- files.
 sed s/' with #Shazam.*'//g <../shazamtags.txt >curllinks.sh #remove everything after "with #Shazam" and creates new file. Overwrites??
 sed -i -- s/' '/'+'/g ./curllinks.sh #remove spaces
 #todo - add the word youtube to the start of the search to reduce the chances of lyrice coming up in search results. There might be a possiblity of adding a !youtube bang to the start of the search. Not sure if this is compatible with I'm Feeling Ducky though.
 sed -i -- s/'^'/'https\:\/\/duckduckgo\.com\/\?q\=\%5C'/ curllinks.sh # add the start of the Duck Duck Go "I'm feeling ducky" search URL
 sed -i -- s/'^'/'curl -Ls -w %{url_effective} '/ curllinks.sh #add the curl command to extract the redirected URL
-sed -i -- s/'$'/' >>encodedlinks.sh'/ curllinks.sh #append the end of the line so that when run, the script will append the results of curl to a file.
+sed -i -- s/'$'/' >>encodedlinks.txt'/ curllinks.sh #append the end of the line so that when run, the script will append the results of curl to a file.
 sed -i -- s/'\&'/'and'/ curllinks.sh #remove special character &
 #todo remove other special characters that will cause sintax error as above with &
 
 #the following lines are my attempt to put the result of each curl on a new line, not sure why it's not working. this is todo.
 sed -i -- s/'$'/'\
-echo -e "\\n" >>encodedlinks.sh'/ curllinks.sh
+echo -e "\\n" >>encodedlinks.txt'/ curllinks.sh
 #sed -i -- s/'$'/'\
 #'/g curllinks.sh 
 
 sed -i -- s/'+\-+'/'+'/g curllinks.sh #remove dashes
 sed -i -- s/[\(\)]/''/g curllinks.sh #remove parenteses, though I thought you didn't have to escape parentesis, so I'm not sure why this works. 
-rm -f encodedlinks.sh #removes existing file and silences errors.
-touch encodedlinks.sh #creates file for curllinks.sh to append to
+rm -f encodedlinks.txt #removes existing file and silences errors.
+touch encodedlinks.txt #creates file for curllinks.sh to append to
 chmod 700 curllinks.sh #set executable
 ./curllinks.sh #executes script
-chmod 700 encodedlinks.sh #sets new script executable
-#echo "Check the encodedlinks.sh file to see that page information looks ok, then hit return to continue"
-#read dummy_variable #wait for user input so I can check the encodedlinks.sh file
-sed -i -- s/'\<html\>.*uddg='/''/g encodedlinks.sh #removes text before encoded URL
-sed -i -- 's/'\'');}set.*$//g' encodedlinks.sh # removes test after enconded URL
+#echo "Check the encodedlinks.txt file to see that page information looks ok, then hit return to continue"
+#read dummy_variable #wait for user input so I can check the encodedlinks.txt file
+sed -i -- s/'\<html\>.*uddg='/''/g encodedlinks.txt #removes text before encoded URL
+sed -i -- 's/'\'');}set.*$//g' encodedlinks.txt # removes test after enconded URL
 
 
 
@@ -39,28 +38,44 @@ sed -i -- 's/'\'');}set.*$//g' encodedlinks.sh # removes test after enconded URL
 ###        #can this be done using printf? http://wiki.bash-hackers.org/commands/builtin/printf
 ###        #convert hex to ascii
 ###sed -e's/%\([0-9A-F][0-9A-F]\)/\\\\\x\1/g' | xargs echo -e
-##sed -e's/%\([0-9A-F][0-9A-F]\)/\\x\1/g' <encodedlinks.sh >temp
+##sed -e's/%\([0-9A-F][0-9A-F]\)/\\x\1/g' <encodedlinks.txt >temp
 ##echo -e "\x41" | awk '{printf "%c\n", $1}'
-##sed -e's/%\([0-9A-F][0-9A-F]\)/\\x\1/g' <encodedlinks.sh >temp
+##sed -e's/%\([0-9A-F][0-9A-F]\)/\\x\1/g' <encodedlinks.txt >temp
 ##echo -e "http\x3A\x2F\x2Fwww.youtube.com\x2Fwatch\x3Fv\x3DjDplwu9kH9Y" | awk '{printf "%c\n", $1}'
 #
 ##Stu helped me find the urldecode.sh script somewhere on the internodes, eg,
 #./urldecode.sh "http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DJGhoLcsr8GA"
 #then I put it to use by looping over the lines according to this page:
 #https://stackoverflow.com/questions/11349827/how-do-i-iterate-through-lines-in-an-external-file-with-shell
-
+rm -f decodedlinks.sh
+#touch decodedlinks.sh
+#echo "cd ~/Desktop" >>decodedlinks.sh
 while read LINK 
 do
         ./urldecode.sh $LINK >>decodedlinks.sh
-done < encodedlinks.sh
+done < encodedlinks.txt
 
+sed -i -- s/'^'/'youtube-dl -x '/ decodedlinks.sh # add the start of the Duck Duck Go "I'm feeling ducky" search URL
+chmod 700 decodedlinks.sh
+brew upgrade youtube-dl
+DIR=$(pwd)
+cd ~/Desktop
+$DIR/decodedlinks.sh
+#./decodedlinks.sh
 
-#todo insert "youtube-dl "
+#cd $DIR
+#rm decodedlinks.sh
+#rm decodedlinks.sh--
+#rm encodedlinks.txt
+#rm encodedlinks.txt--
+#rm curllinks.sh
+#rm curllinks.sh--
+
 #todo insert line to add download to log
 #todo cd to desktop before running script.
-
-#./encodedlinks.sh
-#after I get the curl command in encodedlinks.sh working, I'll need to trim it down, then decode the url to something that youtube-dl can use.
+#todo rm unnecesary files after downloading songs.
+#./encodedlinks.txt
+#after I get the curl command in encodedlinks.txt working, I'll need to trim it down, then decode the url to something that youtube-dl can use.
 
 #now need to get the URL after redirecting from the I'm feeling lucky url generate above. Had problems doing this earlier.
 #https://stackoverflow.com/questions/3074288/get-url-after-redirect
